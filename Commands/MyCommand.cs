e7f9c0ee479d58bdf34f4fd2256e80b50ce09229
Andre Galdino <andre@frgnet.com.br>
2024-01-29 21:28:52 -0300

    adicionado possibilidade de validação por dataannotation

--- Commands/MyCommand.cs
+++ Commands/MyCommand.cs
@@ -1,4 +1,7 @@
 ﻿using EnvDTE;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,8 +10,6 @@ namespace VSIXProject1
     [Command(PackageIds.MyCommand)]
     internal sealed class MyCommand : BaseCommand<MyCommand>
     {
-
-
         protected override void Execute(object sender, EventArgs e)
         {
             DTE dte = (DTE)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
@@ -26,6 +27,25 @@ namespace VSIXProject1
                     string shortTypeName = propertyType.AsString;
 
                     lista.Add((propertyName, typeName, shortTypeName));
+
+                    string propertySourceCode = property.StartPoint.CreateEditPoint().GetText(property.EndPoint.CreateEditPoint());
+
+                    SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(propertySourceCode);
+                    Compilation compilation = CSharpCompilation.Create("MyCompilation")
+                        .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location))
+                        .AddSyntaxTrees(syntaxTree);
+
+                    PropertyDeclarationSyntax propertyDeclaration = syntaxTree.GetRoot().DescendantNodes().OfType<PropertyDeclarationSyntax>().FirstOrDefault();
+
+                    if (propertyDeclaration != null)
+                    {
+                        var dataAnnotations = propertyDeclaration.AttributeLists.SelectMany(list => list.Attributes).ToList();
+                        foreach (AttributeSyntax annotation in dataAnnotations)
+                        {
+                            string annotationName = annotation.Name.ToString();
+                            //aqui voce pode decidir se vai usar ou não a propriedade dependendo do dataAnnotation dela
+                        }
+                    }
                 }
             }
         }
